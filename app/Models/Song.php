009a40972c876cb91424b8d86038e65f546802d9
C. Abdeslam <c.abdeslam@hexaee.com>
2024-04-27 18:18:41 +0100

    add json casts and remove the links validator

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -19,4 +19,16 @@ class Song extends Model
     {
         return $this->belongsTo(Album::class);
     }
+
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'timestamp',
+        'updated_at' => 'timestamp',
+        'links' => 'array',
+    ];
 }
