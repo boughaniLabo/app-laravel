7eb2f5d5b25697e72c760f171d9c04b6b0234ffe
Boughani <abdelghani.demmouche.prof@gmail.com>
2024-04-29 01:20:48 +0100

    help

--- resources/views/page/music.blade.php
+++ resources/views/page/music.blade.php
@@ -4,7 +4,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/2.0.4/wavesurfer.min.js" integrity="sha512-mhqErQ0f2UqnbsjgKpM96XfxVjVMnXpszEXKmnJk8467vR8h0MpiPauil8TKi5F5DldQGqNUO/XTyWbQku22LQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
  
-    function playAudio(song) {
+ function playAudio(song) {
+    
         console.log(song);
         const wavesurfer = WaveSurfer.create({
             container: '#waveform',
@@ -14,12 +15,11 @@
             height: 50,
             cursorWidth: 0,
         });
-        let elementToshow = document.getElementById('costumeWave') ; 
+        let elementToshow = document.getElementById('soundPlayerBig') ; 
         console.log(elementToshow);
         elementToshow.style.display = "block"; // Corrected variable name
         document.getElementById('song-name').innerText = song.title;
-        
-       
+
         // Load audio from the audio element
         wavesurfer.load(song.url);
 
@@ -27,8 +27,9 @@
         wavesurfer.play();
 
         // Set the song title
-     
-  
+    }
+
+    
 
     function closeElement(element) {
         console.log("Function called.");
@@ -41,7 +42,7 @@
             console.log("Parent element not found.");
         }
     }
-} 
+
 </script>
 
 <section class="album">
